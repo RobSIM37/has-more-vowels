def0d8bdbde17b1337ed35e48cf341801696b6f8
Robert Lewis <robsim37@gmail.com>
2021-10-25 09:55:34 -0400

    improved methodology

--- has-more-vowels.js
+++ has-more-vowels.js
@@ -2,13 +2,12 @@ function hasMoreVowels(word) {
 
     const vowels = ['a', 'e', 'i', 'o', 'u']; // Define what a vowel is
     const letters = word.toLowerCase().split(''); // convert the word to an array for easy filtering
+    // reduce the letters down to a single vowelCount by checking if the current letter is included in the vowels and incrimenting the running total if it is.
     const vowelCount = letters.reduce((runningTotal, currentLetter) => vowels.includes(currentLetter) ? ++runningTotal : runningTotal, 0);
-    return vowelCount > letters.length / 2;
+    return vowelCount > letters.length / 2; // return the boolean "vowel count is greater than half the total letters" (it has more vowels)
 
 }
 
-// const vowelArr = letters.filter(currentLetter => vowels.includes(currentLetter)); // filter for all the vowels
-//     return vowelArr.length > letters.length / 2; // return the boolean "the number of vowels is greater than half the total letters" (has more vowels than non-vowels)
 function logResult(word) {
 
     let not = '';
